5fc06f3a1d06df78c166fbd48297187b312bb7c4
NithyaGowri <nithyagkm@gmail.com>
2024-10-09 12:13:36 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello Nithya!", app.getMessage());
+        assertEquals("Hello NithyaGowri!", app.getMessage());
     }
 }
